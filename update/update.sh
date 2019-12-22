36fa793b500dd1c3f2b6ed7679819efd9c0d84b3
Shravan Vasishth <vasishth.shravan@gmail.com>
2019-12-22 10:27:31 +0100

    added books to books header

--- update/update.sh
+++ update/update.sh
@@ -19,11 +19,14 @@ cd ~/Dropbox/Bibliography/
 
 ~/bin/bib2bib -ob vasishthunpub.bib -c 'author : "Vasishth"' -c '$type="UNPUBLISHED" or $type="unpublished"' bibcleaned.bib
 
-~/bin/bib2bib -oc vasishthinproc -ob vasishthinproc.bib -c 'author : "Vasishth"' -c '$type="INPROCEEDINGS" or $type="inproceedings"' -c 'year>2017' bibcleaned.bib
+~/bin/bib2bib -oc vasishthinproc -ob vasishthinproc.bib -c 'author : "Vasishth"' -c '$type="INPROCEEDINGS" or $type="inproceedings"' -c 'year>2018' bibcleaned.bib
+
+~/bin/bib2bib -oc vasishthbooks -ob vasishthinproc.bib -c 'author : "Vasishth"' -c '$type="BOOK" or $type="book" or $type="Book"'  bibcleaned.bib
 
 cp vasishtharticles.bib /Users/shravanvasishth/Git/vasishth.github.io/update/
 cp vasishthunpub.bib /Users/shravanvasishth/Git/vasishth.github.io/update/
 cp vasishthinproc.bib /Users/shravanvasishth/Git/vasishth.github.io/update/
+cp vasishthbooks.bib /Users/shravanvasishth/Git/vasishth.github.io/update/
 
 cd /Users/shravanvasishth/Git/vasishth.github.io/update/
 
@@ -34,38 +37,47 @@ export TMPDIR=.
 ~/bin/bibtex2html -s dsgplain2 -nokeywords  --nobibsource -nf pdf "pdf" -nf code "code" -dl -a -noabstract vasishtharticles.bib
 ~/bin/bibtex2html -s dsgplain2 -nokeywords  --nobibsource -nf pdf "pdf" -dl -a -noabstract vasishthunpub.bib
 ~/bin/bibtex2html -s dsgplain2 -nokeywords  --nobibsource               -dl -a -noabstract vasishthinproc.bib
+~/bin/bibtex2html -s dsgplain2 -nokeywords  --nobibsource               -dl -a -noabstract vasishthbooks.bib
 
 ##strip header junk from bib html files:
 tail -n +16 vasishtharticles.html > tmp.html
 tail -n +16 vasishthunpub.html > tmpunpub.html
 tail -n +16 vasishthinproc.html > tmpinproc.html
+tail -n +16 vasishthbooks.html > tmpbooks.html
 
 
 ## strip last few lines:
 sed '$d' tmp.html | sed '$d' | sed '$d' | sed '$d' > tmp2.html
 sed '$d' tmpunpub.html | sed '$d' | sed '$d' | sed '$d' > tmpunpub2.html
 sed '$d' tmpinproc.html | sed '$d' | sed '$d' | sed '$d' > tmpinproc2.html
+sed '$d' tmpbooks.html | sed '$d' | sed '$d' | sed '$d' > tmpbooks2.html
 
 rm tmp.html
 rm tmpunpub.html
 rm tmpinproc.html
+rm tmpbooks.html
+
 
 cat tmp2.html dl.txt > tmp3.html
 cat unpubheader.txt tmpunpub2.html dl.txt > tmpunpub3.html
 cat inprocheader.txt tmpinproc2.html dl.txt > tmpinproc3.html
+cat booksheader.txt tmpbooks2.html dl.txt > tmpbooks3.html
 
 mv tmp3.html vasishtharticles.html
 mv tmpunpub3.html vasishthunpub.html
 mv tmpinproc3.html vasishthinproc.html
+mv tmpbooks3.html vasishthbooks.html
 
 rm tmp2.html
 rm tmpunpub2.html
 rm tmpinproc2.html
+rm tmpbooks2.html
+
 
 ## move to main web page directory:
 cd ../
 
-cat header2.html update/vasishtharticles.html update/vasishthunpub.html update/vasishthinproc.html  footer2.html > publications.html
+cat header2.html update/vasishtharticles.html update/vasishthunpub.html update/vasishthinproc.html update/vasishthbooks.html footer2.html > publications.html
 
 ##commit index file:
 git commit -m "updated index file" index.html
